680c772b026a804502261d953060d7eb6016a4ed
Orrin Gradwell <OrrinG@miway.co.za>
2020-01-21 09:39:34 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Graphical-Data-Faker
-a Tool that can spoof personal details for use during developement or testing
+
+a Tool that can spoof personal details for use during development or testing.  
+
+For now, this will only spoof:  
+* First Name
+* Last Name
+* RSA ID Number
+* ZA Cell Numbers
+* Email Address  
+
+Long term goal is to extend this to Business details as well as a few usability enhancements.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Graphical-Data-Faker",
+    version="0.0.1",
+    author="Orrin Gradwell",
+    author_email="kamakazy000@gmail.com",
+    description="a Tool that can spoof personal details for use during development or testing.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/OrrinGradwell/Graphical-Data-Faker",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='~=3.7',
+)
